351d9c4d71c2665f43ce18c3ba33955d8e2bcf49
Andy Luis <mvpjava.email@gmail.com>
2024-09-01 17:16:03 +0000

    decoupled terraform and provider config in separate tf files

--- ec2/02-simple-user-data-SG/main.tf
+++ ec2/02-simple-user-data-SG/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.64.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-2"
-}
-
 variable "server_port" {
   description = "SG Inbound port number"
   type = number
