22970caa081d812bebc45d2de15fcfb2ffe02099
Andy Luis <mvpjava.email@gmail.com>
2024-09-06 15:57:35 +0000

    Add subnet outputs and added resources like IG, route table and route table association. now fully working VPC

--- vpc/01-vpc/main.tf
+++ vpc/01-vpc/main.tf
@@ -2,7 +2,15 @@ resource "aws_vpc" "test" {
   cidr_block = var.cidr_block
 
   tags = {
-    env = "demo"
+    Name = "TEST VPC"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = " VPC Internet Gateway"
   }
 }
 
@@ -18,14 +26,33 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-
-  count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.test.id
-  cidr_block        = element(var.private_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
-
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.test.id
+  cidr_block              = element(var.private_subnet_cidrs, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = false
   tags = {
     Name = "Private Subnet ${count.index + 1}"
   }
 
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.test.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+/* Associate public subnets to public route table */
+resource "aws_route_table_association" "this" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
 }
--- vpc/01-vpc/outputs.tf
+++ vpc/01-vpc/outputs.tf
@@ -2,3 +2,13 @@ output "vpc_id" {
   value       = aws_vpc.test.id
   description = "VPC ID"
 }
+
+output "vpc_public_subnets" {
+  value       = aws_subnet.public[*].id
+  description = "List of public subnet created"
+}
+
+output "vpc_private_subnets" {
+  value       = aws_subnet.private[*].id
+  description = "List of private subnet created"
+}
