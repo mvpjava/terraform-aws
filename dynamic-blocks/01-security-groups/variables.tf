e0862b36d44b280c238e269afba07774f6b3073f
mvpjava <mvpjava.email@gmail.com>
2025-10-03 11:16:53 +0000

    added static block example to compare against dynamic

--- dynamic-blocks/01-security-groups/variables.tf
+++ dynamic-blocks/01-security-groups/variables.tf
@@ -25,3 +25,53 @@ variable "secret_key" {
 }
 */
 
+# Ingress rules variable with default
+variable "ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+# Egress rules variable with default
+variable "egress_rules" {
+  description = "List of egress rules for the security group"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
---
+++ dynamic-blocks/01-security-groups/without_dynamic_blocks.tf
@@ -0,0 +1,42 @@
+# This is the old statc way (no dynamic blocks)
+
+# Create Security Group
+resource "aws_security_group" "static_sg" {
+  name        = "static-sg"
+  description = "Security group created with static blocks"
+  vpc_id      = var.vpc_id # !!! TODO replace with your VPC ID !!!
+
+  # Static ingress rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Static egress rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-static-sg"
+  }
+}
